c3de11353ff8d50ae800118555007e3fe427c454
Elliot Toman <web@asubtleweb.com>
2024-09-06 15:27:49 -0400

    Global Monochrome Setting

--- wp-content/themes/zcnyc-2024/snippets/hero.php
+++ wp-content/themes/zcnyc-2024/snippets/hero.php
@@ -1,9 +1,9 @@
 <?php
     $monochrome_setting = get_theme_mod('monochrome_heroes');
     if( $monochrome_setting ) {
-        $hero_type = 'monochrome';
+        $hero_color = 'monochrome';
     } else {
-        $hero_type = 'color';
+        $hero_color = 'color';
     }
     $feautured_image = get_the_post_thumbnail_url( $post->ID, 'full' );
     if( $feautured_image ) {
@@ -13,7 +13,7 @@
     }
 ?>
 
-<div class="hero <?= $hero_type . $monochrome_setting; ?>">
+<div class="hero <?= $hero_type . ' ' . $hero_color; ?>">
     <?php if( $feautured_image ) { ?>
         <div class="hero-image" style="background-image: url(<?php echo $feautured_image; ?>);"></div>
     <?php } ?>
