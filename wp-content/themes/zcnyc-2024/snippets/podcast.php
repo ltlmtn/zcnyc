f7150e5fb781910a366af71edf24b7e8946a793d
Elliot Toman <web@asubtleweb.com>
2024-10-03 16:32:16 -0400

    Add podcast template

---
+++ wp-content/themes/zcnyc-2024/snippets/podcast.php
@@ -0,0 +1,59 @@
+<?php
+    // Template Name: Podcast Page
+    get_header(); 
+?>
+
+<main class="page-content desktop-width">
+    <?php the_content(); ?>
+</main>
+
+<div class="podcast-grid desktop-width">
+
+    <?php
+    //https://stackoverflow.com/questions/37776950/rss-xml-show-itunesimage-url-in-php
+    $doc = new DOMDocument();
+    $doc->load('https://zmm.org/feed/podcast/zcnyc/');
+
+    foreach ($doc->getElementsByTagName('item') as $node) {
+        $itemRSS = array ( 
+            'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
+            'link' => $node->getElementsByTagName('link')->item(0)->nodeValue,
+            'date' => $node->getElementsByTagName('pubDate')->item(0)->nodeValue,
+            'enclosure' => $node->getElementsByTagName('enclosure')->item(0)->getAttribute('url'),
+            'description' => $node->getElementsByTagName('description')->item(0)->nodeValue
+        );
+        $formattedDate = date('F j, Y', strtotime($itemRSS['date']));
+
+    ?>
+        <article>
+            <div class="title">
+                <a href="<?php echo $itemRSS['link']; ?>" target="_blank">
+                    <h3><?php echo $itemRSS['title']; ?> &rarr;</h3>
+                </a>
+            </div>
+            <div class="timestamp"><?php echo $formattedDate; ?></div>
+            <div class="excerpt"><?php echo $itemRSS['description']; ?></div>
+            <div class="audio">
+                <audio controls>
+                    <source src="<?php echo $itemRSS['enclosure']; ?>" type="audio/mpeg">
+                    Your browser does not support the audio element.
+                </audio>
+            </div>
+        </article>
+
+    <?php } ?>
+
+</div>
+
+<script>
+    document.addEventListener('play', function(e){
+        var audios = document.getElementsByTagName('audio');
+        for(var i = 0, len = audios.length; i < len;i++){
+            if(audios[i] != e.target){
+                audios[i].pause();
+            }
+        }
+    }, true);
+</script>
+
+<?php get_footer(); ?>
