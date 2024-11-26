9a3b10d6ef0bd15bba2afaad712f0154d321162c
Elliot Toman <web@asubtleweb.com>
2024-11-26 14:13:47 -0500

    Gallery Carousel

---
+++ wp-content/themes/zcnyc-2024/blocks/lazyblock-gallery-carousel/block.php
@@ -0,0 +1,57 @@
+<?php
+    $autoplay = $attributes['autoplay'];
+    if( $autoplay == 1 ) {
+        $autoplay = 'true';
+    } else {
+        $autoplay = 'false';
+    }
+    $autoplay_speed = $attributes['autoplay_speed'];
+    if( $autoplay_speed == '' ) {
+        $autoplay_speed = 5000;
+    } else {
+        $autoplay_speed = $autoplay_speed * 1000;
+    }
+    $images = $attributes['images'];
+?>
+
+<div class="gallery-carousel">
+    <?php if( $images ) : ?>
+        <?php foreach ($images as $image) : ?>
+            <?php
+                $image_id = $image['id'];
+                $large_image = wp_get_attachment_image_src($image_id, 'large');
+                $full_image = wp_get_attachment_image_src($image_id, 'full');
+                if( $large_image ) {
+                    $image = $large_image[0];
+                } else {
+                    $image = $full_image[0];
+                }
+                $image_alt = $image['alt'];
+                if( $image_alt ) {
+                    $alt = $image_alt;
+                } else {
+                    $alt = 'Gallery Image';
+                }
+            ?>
+            <img src="<?= $image; ?>" alt="<?= $alt; ?>">
+        <?php endforeach; ?>
+    <?php endif; ?>
+</div>
+
+<script>
+    jQuery(document).ready(function($) {
+        $('.gallery-carousel').slick({
+            autoplay: <?= $autoplay; ?>,
+            autoplaySpeed: <?= $autoplay_speed; ?>,
+            arrows: true,
+            dots: false,
+            infinite: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            centerMode: true,
+            variableWidth: true,
+            nextArrow: '<button type="button" class="slick-next"><ion-icon name="chevron-forward-outline"></ion-icon></button>',
+            prevArrow: '<button type="button" class="slick-prev"><ion-icon name="chevron-back-outline"></ion-icon></button>',
+        });
+    });
+</script>
